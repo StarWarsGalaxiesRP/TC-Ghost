b71227cda14676019e0a04d5402e00a5c8cd66d5
Entertainotron <32333348+entertainotron@users.noreply.github.com>
2018-01-28 22:41:01 +0300

    Speed up factories
    Factories sped up by 50%

--- MMOCoreORB/src/server/zone/objects/installation/factory/FactoryObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/installation/factory/FactoryObjectImplementation.cpp
@@ -417,7 +417,7 @@ bool FactoryObjectImplementation::startFactory() {
 			return false;
 	}
 
-	timer = ((int)schematic->getComplexity()) * 8;
+	timer = ((int)schematic->getComplexity()) * 4;
 
 	if(!populateSchematicBlueprint(schematic))
 		return false;
